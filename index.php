c72e6bbd94312d272debd3712beb539cbcea8852
sudo-leo <armellin.1885498@studenti.uniroma1.it>
2021-03-09 18:22:52 +0000

    baing

---
+++ index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html lang="en">
+
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-169630588-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-169630588-1');
+  </script>
+  <script src="/static/js/vendor/jquery.min.220afd743d9e.js"></script>
+  
+  <script src="https://browser.sentry-cdn.com/5.15.5/bundle.min.js"
+    integrity="sha384-wF7Jc4ZlWVxe/L8Ji3hOIBeTgo/HwFuaeEfjGmS3EXAG7Y+7Kjjr91gJpJtr+PAT"
+    crossorigin="anonymous"></script>
+  <script
+    type="text/javascript">Sentry.init({ dsn: 'https://6ce348d3f97e4220a9db5e69183fcd17@o325556.ingest.sentry.io/5245621' });</script>
+  
+  
+  
+  <meta charset="UTF-8">
+  <meta name="archive.org_bot" content="noindex">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" href=/favicon.b963ea651bdb.ico type="image/x-icon">
+  <link rel="apple-touch-icon" sizes="180x180" href=/apple-touch-icon.26d4ca50d3ab.png>
+  <link rel="icon" type="image/png" sizes="32x32" href=/favicon-32x32.aae3a69c5905.png>
+  <link rel="icon" type="image/png" sizes="16x16" href=/favicon-16x16.8a053594bed9.png>
+  <title>
+    Il Blocco: una storia unica
+  </title>
+  <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700,900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href=/style1.css>
+  <link rel="stylesheet" href=/style2.css>
+  <meta name="title" content="Clubhouse: Drop-in audio chat" />
+  <meta name="description" content="Clubhouse is a new type of social network based on voice—where people around the world come together to talk, listen and learn from each other in real-time." />
+  <meta property="og:type" content="website">
+  <meta property="og:site_name" content="Clubhouse" />
+  <meta property="og:title" content="Clubhouse: Drop-in audio chat" />
+  <meta property="og:url" content="https://www.joinclubhouse.com/" />
+  <meta property="og:description" content="Clubhouse is a new type of social network based on voice—where people around the world come together to talk, listen and learn from each other in real-time." />
+  <meta property="og:image" content=https://www.joinclubhouse.com/static/img/full_tn.2c52415a6353.png />
+  <meta name="twitter:card" content="summary" /> 
+  <meta name="twitter:site" content="@joinclubhouse" />
+  <meta name="twitter:title" content="Clubhouse: Drop-in audio chat" />
+  <meta name="twitter:description" content="Clubhouse is a new type of social network based on voice—where people around the world come together to talk, listen and learn from each other in real-time." />
+  <meta property="twitter:url" content="https://www.joinclubhouse.com/" />
+  <meta name="twitter:image" content=https://www.joinclubhouse.com/static/img/summary_tn.97509b5c6693.png />
+  
+  
+  
+  <script type="text/javascript">
+    var banner_error_timeout;
+    var banner_success_timeout;
+    var banner_speed = 'fast';
+    var banner_timeout = 5 * 1000;
+    function show_error(msg) {
+      if (banner_error_timeout) {
+        clearTimeout(banner_error_timeout);
+      }
+      $('#banner_error').text(msg);
+      $('#banner_error').slideDown(banner_speed);
+      banner_error_timeout = setTimeout(function () {
+        $('#banner_error').slideUp(banner_speed);
+      }, banner_timeout);
+    }
+    function show_success(msg) {
+      if (banner_success_timeout) {
+        clearTimeout(banner_success_timeout);
+      }
+      $('#banner_success').text(msg);
+      $('#banner_success').slideDown(banner_speed);
+      banner_success_timeout = setTimeout(function () {
+        $('#banner_success').slideUp(banner_speed);
+      }, banner_timeout);
+    }
+  </script>
+  
+  <script type="text/javascript">
+    (function (e, t) {
+      var n = e.amplitude || { _q: [], _iq: {} }; var r = t.createElement("script")
+        ; r.type = "text/javascript"
+        ; r.integrity = "sha384-RsEu4WZflrqYcEacpfoGSib3qaSvdYwT4D+DrWqeBuDarSzjwUQR1jO8gDiXZd0E"
+        ; r.crossOrigin = "anonymous"; r.async = true
+        ; r.src = "https://cdn.amplitude.com/libs/amplitude-6.2.0-min.gz.js"
+        ; r.onload = function () {
+          if (!e.amplitude.runQueuedFunctions) {
+            console.log("[Amplitude] Error: could not load SDK")
+          }
+        }
+        ; var i = t.getElementsByTagName("script")[0]; i.parentNode.insertBefore(r, i)
+        ; function s(e, t) {
+          e.prototype[t] = function () {
+            this._q.push([t].concat(Array.prototype.slice.call(arguments, 0))); return this
+          }
+        }
+      var o = function () { this._q = []; return this }
+        ; var a = ["add", "append", "clearAll", "prepend", "set", "setOnce", "unset"]
+        ; for (var u = 0; u < a.length; u++) { s(o, a[u]) } n.Identify = o; var c = function () {
+          this._q = []
+            ; return this
+        }
+        ; var l = ["setProductId", "setQuantity", "setPrice", "setRevenueType", "setEventProperties"]
+        ; for (var p = 0; p < l.length; p++) { s(c, l[p]) } n.Revenue = c
+        ; var d = ["init", "logEvent", "logRevenue", "setUserId", "setUserProperties", "setOptOut", "setVersionName", "setDomain", "setDeviceId", "enableTracking", "setGlobalUserProperties", "identify", "clearUserProperties", "setGroup", "logRevenueV2", "regenerateDeviceId", "groupIdentify", "onInit", "logEventWithTimestamp", "logEventWithGroups", "setSessionId", "resetSessionId"]
+        ; function v(e) {
+          function t(t) {
+            e[t] = function () {
+              e._q.push([t].concat(Array.prototype.slice.call(arguments, 0)))
+            }
+          }
+          for (var n = 0; n < d.length; n++) { t(d[n]) }
+        } v(n); n.getInstance = function (e) {
+          e = (!e || e.length === 0 ? "$default_instance" : e).toLowerCase()
+            ; if (!n._iq.hasOwnProperty(e)) { n._iq[e] = { _q: [] }; v(n._iq[e]) } return n._iq[e]
+        }
+        ; e.amplitude = n
+    })(window, document);
+    amplitude.getInstance().init("8c873bbf9534b433b28e5e38bdf2a8fb");
+  </script>
+  
+</head>
+<body class="font-body 
+bg-eggshell
+">
+  <div id="banner_error" class="hidden p-3 text-center text-white bg-red-500" style="display:none"></div>
+  <div id="banner_success" class="hidden p-3 text-center text-white bg-green-500" style="display:none"></div>
+  
+<div class="flex flex-col items-center text-center mx-auto px-5 h-screen">
+  <div class="flex flex-1 flex-col items-center justify-center text-center py-10 text-gray-800 text-xl text-center">
+    <div class="flex items-center justify-center text-center font-bold">
+      <img class="wave object-contain h-10 w-10 pr-1" alt="wave" src="/icon_wave.2872aeea710c.png">
+      Il Blocco
+    </div>
+    <div class="mt-8 text-gray-700 max-w-md">
+      Welcome to the Blocco official page. We are open to new members.
+    </div>
+    <div class="mt-8 px-1 sm:px-14 text-gray-700 max-w-md text-sm">
+      Make a little donation for those bastards who are enjoying life.<br>
+	Dateci li sordi.
+    </div>
+    <div class="mt-8 max-w-sm">
+      <a href="/app"><img src=/appstore.6f955d329ad7.svg /></a>
+    </div>
+  </div>
+  <div class="mt-5">
+    <div class="flex flex-col items-center justify-center w-full py-5 px-8 mb-5 leading-loose font-thin text-center text-xs sm:text-sm text-gray-600">
+    <div class="flex">
+      <a target="_blank"  href="https://www.youtube.com/watch?v=V1bFr2SWP1I">CHI SIAMO (VERAMENTE)</a>
+    </div>
+  </div>
+</div>
+  </div>
+</div>
+</body>
+</html>
